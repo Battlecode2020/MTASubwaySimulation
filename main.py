cd17ac718ca9436471c4285cf5448dffed49e8ea
Student_Zachary_Klein_2023 <zachary78922@gmail.com>
2024-05-03 23:13:06 -0400

    more subway time data

--- main.py
+++ main.py
@@ -94,12 +94,20 @@ if __name__ == '__main__':
     add_line(graph, four_stops, 8, four_times, subwayStations)
 
     # 5 line
-    five_stop_names = []
+    five_stop_names = ["Eastchester-Dyre Av", "Baychester Av", "Gun Hill Rd", "Pelham Pkwy", "Morris Park", "E 180 St", "West Farms Sq-E Tremont Av", "174 St", "Freeman St", "Simpson St", "Intervale Av", "Prospect Av", "Jackson Av", "3 Av-149 St", "149 St-Grand Concourse", "138 St-Grand Concourse", "125 St", "86 St", "59 St", "Grand Central-42 St", "14 St-Union Sq", "Brooklyn Bridge-City Hall", "Fulton St", "Wall St", "Bowling Green", "Borough Hall", "Nevins St", "Atlantic Av-Barclays Ctr", "Franklin Av-Medgar Evers College", "President St-Medgar Evers College", "Sterling St", "Winthrop St", "Church Av", "Beverly Rd", "Newkirk Av-Little Haiti", "Flatbush Av-Brooklyn College"]
     five_stops = []
-
-    five_times = []
+    for i in five_stop_names:
+        five_stops.append(find_stations(subwayStations, i, "5"))
+    five_times = [1,3,2,2,3,2,1,2,1,2,1,2,3,1,3,2,5,3,2,3,6,2,1,2,5,2,1,5,2,2,2,2,1,1,3]
+    add_line(graph, five_stops, 8, five_times, subwayStations)
 
     # 6 line
+    six_stop_names = ["Pelham Bay Park", "Buhre Av", "Middletown Rd", "Westchester Sq-E Tremont Av", "Zerega Av", "Castle Hill Av", "Parkchester", "St Lawrence Av", "Morrison Av-Soundview", "Elder Av", "Whitlock Av", "Hunts Point Av", "Longwood Av", "E 149 St", "E 143 St-St Mary's St", "Cypress Av", "Brook Av", "3 Av-138 St", "125 St", "116 St", "110 St", "103 St", "96 St", "86 St", "77 St","68 St-Hunter College","59 St", "51 St", "Grand Central-42 St", "33 St", "28 St", "23 St", "14 St-Union Sq", "Astor Pl", "Bleecker St", "Spring St", "Canal St", "Brooklyn Bridge-City Hall"]
+    six_stops = []
+    for i in six_stop_names:
+        six_stops.append(find_stations(subwayStations, i, "6"))
+    six_times = [2,2,2,2,1,1,1,2,1,1,2,1,1,1,2,2,2,3,1,1,2,2,2,2,1,2,1,2,2,1,1,1,1,3,1,1,2]
+    add_line(graph, six_stops, 8, six_times, subwayStations)
 
     # 7 line
 
